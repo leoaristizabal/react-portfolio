9b50034353cc595faac71dcedcfdf6d0e30e5564
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-08-26 20:06:16 -0300

    finish sidebar rrss structure

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoL from '../../assets/images/logo-L.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => (
@@ -15,20 +15,70 @@ const Sidebar = () => (
             <img className="sub-logo" src={LogoSubtitle} alt="LeoAristizabal"/>
         </Link>
         <nav>
-            <NavLink exact= "true" activeclassname="active" to = "/">
+
+            <NavLink 
+                exact= "true" 
+                activeclassname="active" 
+                to = "/"
+            >
                 <FontAwesomeIcon icon= {faHome} color ="#4d4d4e"/>
             </NavLink>
-            <NavLink exact= "true" activeclassname="active" className="about-link" to = "/">
+            
+            <NavLink
+             exact= "true" 
+             activeclassname="active" 
+             className="about-link" 
+             to = "/about"
+             >
+
                 <FontAwesomeIcon icon= {faUser} color ="#4d4d4e"/>
             </NavLink>
-            <NavLink exact= "true" activeclassname="active" className="contact-link" to = "/">
-                <FontAwesomeIcon icon= {faEnvelope} color ="#4d4d4e"/>
+
+            <NavLink 
+            exact= "true" 
+            activeclassname="active" 
+            className="contact-link" 
+            to = "/contact"
+            >
+            <FontAwesomeIcon icon= {faEnvelope} color ="#4d4d4e"/>
             </NavLink>
+
         </nav>
         <ul>
             <li>
-                <a target="_blank" rel="noreferrer" href='https://github.com/leoaristizabal' >
+                <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href='https://github.com/leoaristizabal' 
+                >
                     <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/> 
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href='https://instagram.com/leoaristizabal' 
+                >
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"/> 
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href='https://github.com/leoaristizabal' 
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/> 
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href='https://wa.link/7piiy0' 
+                >
+                    <FontAwesomeIcon icon={faWhatsapp} color="#4d4d4e"/> 
                 </a>
             </li>
         </ul>
