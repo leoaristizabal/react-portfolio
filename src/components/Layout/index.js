5845dfa3d38da7c64ec7c89eb7ac14981828542b
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-08-26 20:07:11 -0300

    tags structure for home page layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,25 @@
 import './index.scss';
 import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-    <>
+    <div className="App">
         <Sidebar/>
-    </>
+        <div clasName="page">
+            <span className= "tags top-tags">&lt;body&gt;</span>
+
+            <Outlet/>
+
+            <span className='tags bottom-tags'>&lt;/body&gt;
+            <br/>
+            <span className='bottom-tag-html'>&lt;/html&gt;</span>
+            
+            </span>
+
+
+        </div>
+    </div>
 
         )
 }
